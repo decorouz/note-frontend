cf93155e09bef2d8d97a2cc994372230075795b8
Biola <abiola2326@gmail.com>
2020-04-08 19:59:18 +0100

    updated note.js

--- src/service/notes.js
+++ src/service/notes.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
-const baseUrl = 'http://localhost:3003/api/notes';
+const baseUrl = '/api/notes';
 
 const getAll = () => {
   const request = axios.get(baseUrl);
+
   return request.then(response => response.data);
 };
 
